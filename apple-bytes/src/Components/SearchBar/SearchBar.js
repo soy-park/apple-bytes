d43e49102b7d101601b11d91f034286ec987df1b
Sara Park <sspark106@gmail.com>
2023-07-24 17:31:00 -0400

    feat: Add handleChange function that sets the state of input to input value

--- apple-bytes/src/Components/SearchBar/SearchBar.js
+++ apple-bytes/src/Components/SearchBar/SearchBar.js
@@ -4,15 +4,20 @@ import '../SearchBar/SearchBar.css';
 const SearchBar = () => {
     const [input, setInput] = useState("");
 
+    const handleChange = (event) => {
+        setInput(event.target.value)
+    }
+
     return (
       <div className="searchbar-container">
         <input 
             type="text"
             id="article"
             name="article"
-            
-
+            value={input}
+            onChange={handleChange}
         />
+        <button className="search-btn" onClick={handleSearch}>Search</button>
       </div>
     );
   }
