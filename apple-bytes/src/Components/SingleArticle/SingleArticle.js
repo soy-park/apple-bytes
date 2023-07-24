e19ef6f1c873296aeb57b2bb9d75637d3acc2ba1
Sara Park <sspark106@gmail.com>
2023-07-24 19:21:06 -0400

    feat: Add HTML outline with JSX syntax

--- apple-bytes/src/Components/SingleArticle/SingleArticle.js
+++ apple-bytes/src/Components/SingleArticle/SingleArticle.js
@@ -3,13 +3,19 @@ import '../SingleArticle/SingleArticle.css';
 
 const SingleArticle = ({ chosenTitle, articles }) => {
     const chosenArticle = articles.find(article => article.title.replace(/"/g, '') === chosenTitle.replace(/"/g, ''));
-    
+
     return (
         <main className="detailed-article-view">
             <div className="article-button-container">
                 <button onClick={}>All Articles</button>
             </div>
-            <h2 className="headline"></h2>
+            <h2 className="headline">{chosenArticle.title}</h2>
+            <div className="img-container">
+                <img src={chosenArticle.urlToImage} alt="Article image"/>
+            </div>
+            <p className="date">{chosenArticle.publishedAt.split('T')[0]}</p>
+            <h5 className="content">{chosenArticle.content}</h5>
+            <p className="source">{chosenArticle.source.name}</p>
         </main>
     )
 }
