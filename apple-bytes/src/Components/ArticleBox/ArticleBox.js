e9458ecc30689f1dae91dff2bfb965800b4bc900
Sara Park <sspark106@gmail.com>
2023-07-24 18:23:13 -0400

    feat: Add img, h3, and p elements for article image, headline, date, and description

--- apple-bytes/src/Components/ArticleBox/ArticleBox.js
+++ apple-bytes/src/Components/ArticleBox/ArticleBox.js
@@ -4,8 +4,11 @@ import { NavLink } from 'react-router-dom';
 
 const ArticleBox = ({ image, headline, date, description, content, source, url, setCurrentPage }) => {
     return (
-        <NavLink to={}>
-            <h2></h2>
+        <NavLink to={url}>
+            <img src={image} alt="Article image" className="article-image"></img>
+            <h3 className="headline">{headline}</h3>
+            <p className="date">{date}</p>
+            <p className="description">{description}</p>
         </NavLink>
     )
 }
