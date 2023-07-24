08474665fe0919c30d90c353059e95287f91a44c
Sara Park <sspark106@gmail.com>
2023-07-24 16:05:44 -0400

    feat: Pass currentPage as prop for AllArticles and SingleArticle

--- apple-bytes/src/App.js
+++ apple-bytes/src/App.js
@@ -6,8 +6,8 @@ const App = () => {
   return (
     <div className="App">
       <Header />
-      {currentPage === 'home' && <AllArticles />}
-      {currentPage === 'article' && <SingleArticle />}
+      {currentPage === 'home' && <AllArticles currentPage={currentPage} />}
+      {currentPage === 'article' && <SingleArticle currentPage={currentPage} />}
     </div>
   );
 }
