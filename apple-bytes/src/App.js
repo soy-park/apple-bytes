145f325c0ba1dc88e582abc5ced31d0513a7f098
Sara Park <sspark106@gmail.com>
2023-07-24 16:37:17 -0400

    feat: Pass articles as props in AllArticles and SingleArticle

--- apple-bytes/src/App.js
+++ apple-bytes/src/App.js
@@ -7,13 +7,15 @@ const App = () => {
   const [currentPage, setCurrentPage] = useState('home');
   const [articles, setArticles] = useState([]);
 
-  
+  getArticles()
+  .then(data => setArticles(data.articles))
+  .catch(error => {throw new Error(error)})
 
   return (
     <div className="App">
       <Header />
-      {currentPage === 'home' && <AllArticles setCurrentPage={setCurrentPage} />}
-      {currentPage === 'article' && <SingleArticle setCurrentPage={setCurrentPage} />}
+      {currentPage === 'home' && <AllArticles setCurrentPage={setCurrentPage} articles={articles} />}
+      {currentPage === 'article' && <SingleArticle setCurrentPage={setCurrentPage} articles={articles} />}
     </div>
   );
 }
