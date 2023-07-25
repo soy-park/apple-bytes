1cdcf1565a49a202c410449b2d6c1ae5c3436ab9
Sara Park <sspark106@gmail.com>
2023-07-25 18:01:46 -0400

    feat: Add media query for desktop screen sizes

--- apple-bytes/src/Components/SingleArticle/SingleArticle.js
+++ apple-bytes/src/Components/SingleArticle/SingleArticle.js
@@ -17,11 +17,11 @@ const SingleArticle = ({ chosenTitle, articles }) => {
             </div>
             <h2 className="headline">{chosenArticle.title}</h2>
             <div className="img-container">
-                <img src={chosenArticle.urlToImage} alt="Article image"/>
+                <img src={chosenArticle.urlToImage} alt="Article image" className="article-img"/>
             </div>
             <p className="date">{chosenArticle.publishedAt.split('T')[0]}</p>
             <h5 className="content">{chosenArticle.content}</h5>
-            <h5>For more content, visit <a href={chosenArticle.url}>{chosenArticle.url}</a></h5>
+            <h5 className='more-content'>For more content, visit <a href={chosenArticle.url}>{chosenArticle.url}</a></h5>
             <p className="source">{chosenArticle.source.name}</p>
         </main>
     )
