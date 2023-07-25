abaecfd78345989bc2e386505b790f635f2cef97
Sara Park <sspark106@gmail.com>
2023-07-25 17:08:11 -0400

    feat: Add flexbox to article img to align it in the center, and margins of article container

--- apple-bytes/src/Components/ArticleBox/ArticleBox.js
+++ apple-bytes/src/Components/ArticleBox/ArticleBox.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 const ArticleBox = ({ image, headline, date, description, content, source }) => {
     return (
         <article>
-            <NavLink to={`/${headline}`}>
+            <NavLink to={`/${headline}`} className="link-to-single-article">
                 <img src={image} alt="Article image" className="article-image"/>
             </NavLink>
             <h3 className="headline">{headline}</h3>
