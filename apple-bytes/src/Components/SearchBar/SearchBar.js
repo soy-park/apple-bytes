e421090b8525543eca69de2e6e2c7a21aea9e0a9
Sara Park <sspark106@gmail.com>
2023-07-26 17:22:22 -0400

    feat: Add useEffect() hook for asynchronous operations

--- apple-bytes/src/Components/SearchBar/SearchBar.js
+++ apple-bytes/src/Components/SearchBar/SearchBar.js
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../SearchBar/SearchBar.css';
 
 const SearchBar = ({ setArticles, articles }) => {
     const [input, setInput] = useState("");
 
+    useEffect = (() => {
+        handleSearch()
+    }, [input]);
+
     const handleChange = (event) => {
-        if (!event.target.value) {
-            return setInput("");
-            // setArticles(articles);
-        } else {
-            return setInput(event.target.value);
-        }
+        const searchTerm = event.target.value;
+        setInput(searchTerm);
     }
 
-    const handleSearch = (event) => {
-        event.preventDefault();
+    const handleSearch = () => {
         if (!input) {
-            return alert("Please provide a search term")
+            setArticles(articles);
+            return alert("Please provide a search term");
         } else {
             const filteredArticles = articles.filter(article => (article.title.toLowerCase().includes(input.toLowerCase()) || article.description.toLowerCase().includes(input.toLowerCase()) || article.publishedAt.split('T')[0].toLowerCase().includes(input.toLowerCase())))
             return setArticles(filteredArticles);
@@ -31,6 +31,7 @@ const SearchBar = ({ setArticles, articles }) => {
             name="article"
             value={input}
             onChange={(event) => handleChange(event)}
+            className="search-field"
         />
         <button className="search-btn" onClick={handleSearch}>Search</button>
       </div>
